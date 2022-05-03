fe489a4a8e3a62c306682b716c24dfe31addbd64
theultimat <theultimat@gmail.com>
2022-05-03 19:53:06 +0100

    Main: Initial main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
+#include "assert.hpp"
+#include "graphics_context.hpp"
+#include "graphics_window.hpp"
+#include "trace.hpp"
+
+
+VHS_TRACE_DEFINE(MAIN);
+
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    VHS_TRACE(MAIN, "Starting initialisation.");
+
+    vhs::GraphicsContext context;
+
+    auto window = context.graphics_window();
+
+    VHS_TRACE(MAIN, "Initialisation complete, entering main loop.");
+
+    while (window.is_open())
+    {
+        window.poll_events();
+    }
+
+    VHS_TRACE(MAIN, "Window closing, main loop exited.");
+
     return 0;
 }

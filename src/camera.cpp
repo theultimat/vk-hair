9e66f69bdb0cdf74329339f27292d12cd8005f15
theultimat <theultimat@gmail.com>
2022-05-15 13:06:59 +0100

    Camera: Initial implementation.
    - Simple FPS style camera.
    - Uses pitch/yaw angles instead of quaternions as for the purposes of
      this application the former does the job.

---
+++ src/camera.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "camera.hpp"
+#include "io.hpp"
+
+
+namespace vhs
+{
+    Camera::Camera(uint32_t window_width, uint32_t window_height, const glm::vec3& position) :
+        position_ { position }
+    {
+        auto aspect_ratio = (float)window_width / window_height;
+        project(glm::radians(90.0f), aspect_ratio, 0.1f, 100.0f);
+        update(0);
+    }
+
+
+    void Camera::project(float fov, float aspect_ratio, float near, float far)
+    {
+        projection_ = glm::perspective(fov, aspect_ratio, near, far);
+        projection_[1][1] *= -1;
+    }
+
+
+    void Camera::process_input(const KeyboardState& ks, float dx, float dy)
+    {
+        move_ = glm::vec3 { 0 };
+
+        if (ks.down(GLFW_KEY_W))
+            move_ += front_;
+        else if (ks.down(GLFW_KEY_S))
+            move_ -= front_;
+
+        if (ks.down(GLFW_KEY_A))
+            move_ -= right_;
+        else if (ks.down(GLFW_KEY_D))
+            move_ += right_;
+
+        if (move_.x || move_.y)
+            move_ = glm::normalize(move_);
+
+        move_ *= speed_;
+
+        pitch_move_ = -dy * sensitivity_;
+        yaw_move_ = dx * sensitivity_;
+    }
+
+    void Camera::update(float dt)
+    {
+        pitch_ += pitch_move_ * dt;
+        yaw_ += yaw_move_ * dt;
+
+        glm::vec3 front;
+
+        front.x = std::cos(pitch_) * std::cos(yaw_);
+        front.y = std::sin(pitch_);
+        front.z = std::cos(pitch_) * std::sin(yaw_);
+
+        front_ = glm::normalize(front);
+        right_ = glm::normalize(glm::cross(front_, glm::vec3 { 0, 1, 0 }));
+        up_ = glm::normalize(glm::cross(right_, front_));
+
+        position_ += move_ * dt;
+        view_ = glm::lookAt(position_, position_ + front_, up_);
+    }
+}
---
+++ src/camera.hpp
@@ -0,0 +1,62 @@
+#ifndef VHS_CAMERA_HPP
+#define VHS_CAMERA_HPP
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+
+namespace vhs
+{
+    class KeyboardState;
+
+    // Simple first person style camera class.
+    class Camera
+    {
+    public:
+        Camera() = delete;
+        Camera(const Camera&) = default;
+        Camera(Camera&&) = default;
+        ~Camera() = default;
+
+        Camera(uint32_t window_width, uint32_t window_height, const glm::vec3& position);
+
+
+        Camera& operator=(const Camera&) = default;
+        Camera& operator=(Camera&&) = default;
+
+
+        // Calculate the projection matrix.
+        void project(float fov, float aspect_ratio, float near, float far);
+
+        // Process input and update.
+        void process_input(const KeyboardState& ks, float dx, float dy);
+        void update(float dt);
+
+        // Accessors.
+        const glm::mat4& projection() const { return projection_; }
+        const glm::mat4& view() const { return view_; }
+        const glm::vec3& position() const { return position_; }
+        const glm::vec3& front() const { return front_; }
+        const glm::vec3& right() const { return right_; }
+
+        float pitch() const { return pitch_; }
+        float yaw() const { return yaw_; }
+
+    private:
+        glm::mat4 projection_;
+        glm::mat4 view_;
+        glm::vec3 position_;
+        glm::vec3 front_;
+        glm::vec3 right_;
+        glm::vec3 up_;
+        glm::vec3 move_;
+        float pitch_ = 0;
+        float yaw_ = 0;
+        float pitch_move_ = 0;
+        float yaw_move_ = 0;
+        float speed_ = 1;
+        float sensitivity_ = 1;
+    };
+}
+
+#endif

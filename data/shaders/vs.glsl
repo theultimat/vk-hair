fa622110a7db9d0be36b5105625425e69faeb898
theultimat <theultimat@gmail.com>
2022-05-29 11:27:02 +0100

    Data: Make hairs random colour in vertex shader for debug.

--- data/shaders/vs.glsl
+++ data/shaders/vs.glsl
@@ -9,10 +9,26 @@ layout (push_constant) uniform PushConstants
     mat4 ModelViewProjection;
 } u_PushConstants;
 
+float rand(vec2 seed)
+{
+    return fract(sin(dot(seed, vec2(12.989, 78.233))) * 43758.5453);
+}
+
+vec3 randcol(uint id)
+{
+    vec3 col;
+
+    col.r = rand(vec2(id, id + 1000));
+    col.g = rand(vec2(id, id + 2000));
+    col.b = rand(vec2(id, id + 3000));
+
+    return col;
+}
+
 void main()
 {
     gl_Position = u_PushConstants.ModelViewProjection * vec4(vsIn_Position, 1);
-    vsOut_Colour = vec3(1, 0, 1);
+    vsOut_Colour = randcol(gl_VertexIndex / 8);
 
     gl_PointSize = 8.0f;
 }
